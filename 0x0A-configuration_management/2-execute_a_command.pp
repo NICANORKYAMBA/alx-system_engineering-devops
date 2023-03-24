437d1bb2787f3574f3df091aba864a1345ca3658
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-03-24 12:27:25 +0300

    Execute a command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # Use the "exec" resource type to execute the "pkill" command to kill the process
 exec { 'killmenow':
-  command     => 'pkill -9 -f killmenow',
+  command => 'pkill -9 killmenow',
   path    => ['/usr/bin', '/usr/sbin', '/bin']
 }
