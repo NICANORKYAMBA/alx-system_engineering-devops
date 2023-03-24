aec5f5a44cf81581e1ac4c69ede2a50ea07cd15f
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-03-24 12:10:51 +0300

    Execute a command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # Use the "exec" resource type to execute the "pkill" command to kill the process
 exec { 'killmenow':
-  command     => '/usr/bin/pkill killmenow',
+  command     => 'pkill killmenow',
   # Set the "refreshonly" parameter to true to ensure that the command is only executed when triggered by another resource
   refreshonly => true,
 }
