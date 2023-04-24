8d065e48db400476c76672a3d0103756a37c4eed
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-04-24 14:35:19 +0300

    Fixing Errors

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,32 +8,34 @@ import requests
 import sys
 
 
-BASE_URL = 'https://jsonplaceholder.typicode.com'
-employee_id = int(sys.argv[1])
+if __name__ == '__main__':
+    
+    BASE_URL = 'https://jsonplaceholder.typicode.com'
+    employee_id = int(sys.argv[1])
 
-"""Make a GET request to retrieve the employee's TODO list"""
-response = requests.get('{}/todos?userId={}'.format(
-    BASE_URL, employee_id))
+    """Make a GET request to retrieve the employee's TODO list"""
+    response = requests.get('{}/todos?userId={}'.format(
+        BASE_URL, employee_id))
 
-"""Parse the JSON response into a Python list of dictionaries"""
-todos = response.json()
+    """Parse the JSON response into a Python list of dictionaries"""
+    todos = response.json()
 
-"""Calculate the number of completed and total tasks"""
-total_tasks = len(todos)
+    """Calculate the number of completed and total tasks"""
+    total_tasks = len(todos)
 
-completed_tasks = sum(1 for todo in todos if todo['completed'])
+    completed_tasks = sum(1 for todo in todos if todo['completed'])
 
-"""Get employee name from API"""
-user_id = todos[0]['userId']
-response = requests.get('{}/users/{}'.format(BASE_URL, user_id))
-employee = response.json()
-employee_name = employee['name']
+    """Get employee name from API"""
+    user_id = todos[0]['userId']
+    response = requests.get('{}/users/{}'.format(BASE_URL, user_id))
+    employee = response.json()
+    employee_name = employee['name']
 
-"""Display the progress information"""
-print("Employee {} is done with tasks({}/{}):".format(
-    employee_name, completed_tasks, total_tasks))
+    """Display the progress information"""
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_name, completed_tasks, total_tasks))
 
-"""Display the titles of completed tasks"""
-for todo in todos:
-    if todo['completed']:
-        print('\t{}'.format(todo['title']))
+    """Display the titles of completed tasks"""
+    for todo in todos:
+        if todo['completed']:
+            print('\t{}'.format(todo['title']))
