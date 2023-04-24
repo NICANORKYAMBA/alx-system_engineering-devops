df4dc11ba9202c52ad37ccf0e677497987eb2e2b
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-04-24 14:38:25 +0300

    Fixing Errors

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -11,6 +11,12 @@ import sys
 if __name__ == '__main__':
 
     BASE_URL = 'https://jsonplaceholder.typicode.com'
+
+    """Get the employee ID from the command-line arguments"""
+    if len(sys.argv) < 2:
+        print('Please provide an employee ID as an argument.')
+        sys.exit(1)
+    
     employee_id = int(sys.argv[1])
 
     """Make a GET request to retrieve the employee's TODO list"""
