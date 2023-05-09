497a06b7eb68d6e23ac78640e184ac0f8ed1052a
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-05-09 15:44:07 +0300

    Fixing Errors

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -6,79 +6,46 @@ articles, and prints a sorted count of given keywords
 import requests
 
 
-def count_words(subreddit, word_list, after='', counts={}):
+def count_words(subreddit, word_list, instances={}, after="", count=0):
     """
-    Queries the Reddit API, parses the title of all hot articles,
-    and prints a sorted count of given keywords.
-
-    Args:
-    - subreddit (str): the subreddit to search for
-    - word_list (list): a list of keywords to count
-    - after (str): the identifier of the post to start the search after
-    - counts (dict): a dictionary of counts for each keyword
-
-    Returns:
-    - None
-
-    Note:
-    - If word_list contains the same word (case-insensitive),
-    the final count should be the sum of each duplicate.
-    - Results should be printed in descending order,
-    by the count, and if the count is the same for separate keywords,
-    they should then be sorted alphabetically (ascending, from A to Z).
-    - Words with no matches should be skipped and not printed.
-    Words must be printed in lowercase.
-    - Results are based on the number of times a keyword appears,
-    not titles it appears in.
+    Prints counts of given words found in hot posts of a given subreddit.
     """
-    # Base case: if subreddit is invalid or there are no more posts
-    if subreddit is None or not after:
-        # Sort and print the word counts
-        for word, count in sorted(counts.items(), key=lambda x: (-x[1], x[0])):
-            print("{}: {}".format(word, count))
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "myUserAgent1.0"
+    }
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    try:
+        results = response.json()
+        if response.status_code == 404:
+            raise Exception
+    except Exception:
         return
 
-    # Define the Reddit API endpoint
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    results = results.get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        title = c.get("data").get("title").lower().split()
+        for word in word_list:
+            if word.lower() in title:
+                times = len([t for t in title if t == word.lower()])
+                if instances.get(word) is None:
+                    instances[word] = times
+                else:
+                    instances[word] += times
 
-    # Define the custom User-Agent to avoid Too Many Requests error
-    headers = {'User-Agent': 'Mozilla/5.0'}
-
-    # Define the parameters for the Reddit API request
-    params = {'after': after}
-
-    # Send the request to the Reddit API
-    response = requests.get(url, headers=headers, params=params)
-
-    # Check if the request was successful
-    if response.status_code == 200:
-        # Parse the response JSON
-        data = response.json()
-
-        # Get the next post identifier for recursive calls
-        next_after = data['data']['after']
-
-        # Get the list of hot posts
-        posts = data['data']['children']
-
-        # Process each hot post
-        for post in posts:
-            # Get the title of the post
-            title = post['data']['title']
-
-            # Split the title into words
-            words = title.lower().split()
-
-            # Count the occurrences of each keyword in the title
-            for word in word_list:
-                # Check if the keyword is in the title
-                if word.lower() in words:
-                    # Add the keyword to the count dictionary
-                    counts[word.lower()] = counts.get(word.lower(), 0) + 1
-
-        # Recursive call with the next post identifier
-        count_words(subreddit, word_list, next_after, counts)
-
+    if after is None:
+        if len(instances) == 0:
+            print("")
+            return
+        instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
+        [print("{}: {}".format(k, v)) for k, v in instances]
     else:
-        # Subreddit is invalid, print nothing
-        print(None)
+        count_words(subreddit, word_list, instances, after, count)
