fd8799d65d6845a692e0cf88c5b7fb5aa7e83a51
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-05-18 23:34:04 +0300

    sky is the limit not

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,8 @@
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
-}
-
--> exec { 'nginx-restart':
+} -> exec { 'nginx-restart':
   command => 'nginx restart',
-  path    => '/etc/init.d/'
+  path    => '/etc/init.d/',
+  subscribe => Exec['fix--for-nginx'],
 }
